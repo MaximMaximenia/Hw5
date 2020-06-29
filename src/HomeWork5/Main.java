71e4efd68cc2057afc0f54b28f66678fdc2f8cc1
Максим <maxich66@gmail.com>
2020-06-29 10:25:44 +0300

    была ошибка.

--- src/HomeWork5/Main.java
+++ src/HomeWork5/Main.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         PassengerTransport n = new PassengerTransport();
-        n.setBrand("3%");
+        n.setBrand("Suzuki");
         n.setMaxSpeed(100);
         n.setPower(54);
+        n.setFuelConsumption(222);
         n.info();
         n.kilMaxSpeed(12);
-
 
     }
 }
